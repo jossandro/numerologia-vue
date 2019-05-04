27637e663fe1ebcad81812cc201544a3d7fc0f8a
Jossandro Balardin <jossandro@gmail.com>
2019-05-03 21:35:59 -0300

    new page for personal numerology

---
+++ data.js
@@ -0,0 +1,72 @@
+var soul = {
+	1 : 'Você traz um grande espírito de iniciativa e liderança, desenvolvido ao longo das suas vidas passadas. ' +
+		'Você vivenciou um grande destaque na sociedade, nos negócios ou até no esporte e na cultura.' + 
+		'Você gosta de traçar seus caminhos e prefere agir sozinho, tendo dificuldade em aceitar a opinião dos outros. Você aprendeu a ser individualista, lutando obstinadamente pelas suas convicções. Não suporta imposições, preferindo estabelecer e seguir suas próprias regras.' +
+        'A determinação e a força de vontade fizeram de você um pioneiro que aprecia as novidades. Você deve aproveitar sua grande força interior, o que o capacita a tomar iniciativas e a liderar as empreitadas. Deve estar atento para não cair na tentação de achar que sabe tudo e não precisa da contribuição dos outros, podendo assim tornar-se egoísta, dominador e intransigente.',
+    2 : 'Ao longo de suas vidas passadas, aprendeu a procurar sempre uma solução para os problemas, garantindo assim a harmonia no lar, no trabalho ou mesmo nos seus relacionamentos pessoais. Amante da paz, trabalhou muito bem em grupo, normalmente sob o comando de um líder.' +
+        'Sua alma lhe confere uma excepcional capacidade de discernimento, através da qual consegue separar o joio do trigo sem dificuldade. Sabe guardar segredos e dar conselhos como ninguém. Sua gentileza e amabilidade atraem principalmente aqueles que nos momentos difíceis procuram um ombro amigo.' +
+        'Você deve tirar proveito desse seu potencial de companheirismo e diplomacia para obter sucesso em tudo aquilo que faz, evitando assim que a indecisão e a insegurança atrapalhem a concretização de seus planos.',
+    3 : 'Ao longo de suas vidas passadas, aprendeu a não se abater diante dos problemas e sempre procurou encontrar o lado bom da vida. A alegria, o espírito desarmado e o otimismo foram seus traços marcantes.' +
+        'Ser feliz é fazer os outros felizes: esse é o seu lema. Seu sorriso estampado no rosto e seu estilo espirituoso são envolventes. Desenvolveu um grande poder de comunicação e uma criatividade aguçada. Nos momentos mais difíceis consegue minimizar o sofrimento, confortando os mais entristecidos. Se o ambiente estiver descontraído, melhor ainda: aí é que você se solta, contagiando os que estão à volta.' +
+        'Você deve capitalizar seu bom humor e otimismo com vistas a atingir os seus objetivos, evitando a superficialidade, a mentira, o narcisismo e a propensão a fazer fofocas, defeitos que podem se manifestar a qualquer hora.'
+};
+ 
+NÚMERO DA ALMA – 4
+            Ao longo de suas vidas passadas, acumulou um grande espírito prático voltado ao trabalho bem feito. Exigente e perfeccionista, aprendeu a agir de forma organizada e sem precipitação.
+            Aprendeu a trabalhar com eficiência e colhendo bons resultados, pois costuma lograr êxito, principalmente material, em tudo aquilo que faz. Desde a decoração de sua casa até o desenvolvimento de um projeto, é cauteloso e não costuma se envolver em aventuras perigosas. Aprendeu a ser prudente, considerando primeiro os obstáculos a serem transpostos para depois avaliar os benefícios.
+            Você deve assumir aquilo que verdadeiramente é: Trabalhador, eficiente e meticuloso. Não deve ficar preocupado com acusações e ciúmes, caso contrário pode enveredar para o caminho da improdutividade, negligência e medo.
+ 
+  
+NÚMERO DA ALMA – 5
+Você traz um amor pela liberdade desenvolvido ao longo de suas vidas passadas. Nunca aceitou que alguém interferisse nas suas decisões. Ser feliz, para você, é poder ir e vir. O excesso de rotina o deixa irritado, pois você prefere o inusitado, o surpreendente e o inesperado.
+            Foge sempre da monotonia; seu desejo insaciável de viajar vem da sua vivência e da sua necessidade de liberdade. É atraído por novas perspectivas. Aventuras não-convencionais o seduzem.
+            Você desenvolveu muitos talentos e possui diversos interesses. Sua mente é brilhante, e sua curiosidade faz com que explore novas idéias sem preconceitos. Deve tomar cuidado caso esteja com dificuldade momentânea de expressar esse seu jeito de ser, para não sucumbir ao vício (drogas, alcoolismo, jogo, etc.) e a atitudes irresponsáveis.
+ 
+ 
+NÚMERO DA ALMA – 6
+            Você traz um espírito de enorme responsabilidade voltado para a harmonia, desenvolvido ao longo de suas vidas passadas. Fiel no casamento, dedica-se com louvor ao bem-estar e à felicidade daqueles que o cercam.
+            Tem uma sensibilidade muito aguçada e é capaz de restabelecer a paz entre aqueles que estão à sua volta. Cumpre seus deveres com primazia e não foge aos seus compromissos. Prefere trabalhar em grupo a trabalhar de forma individual.
+            Você deve aproveitar o dom que possui e construir assim um mundo mais harmônico e justo. No entanto, tome cuidado para não se tornar um possessivo.
+ 
+ 
+ 
+NÚMERO DA ALMA – 7
+            Você traz a sabedoria e a justiça desenvolvidas ao longo de suas vidas passadas. Procurou sempre analisar os fatos com profundidade para então chegar a uma conclusão que o levasse a ações seguras.
+            Não gosta que pairem dúvidas sobre suas atitudes. Consegue através da busca da espiritualidade uma forma de iluminar os seus caminhos. A retidão na conduta é mais importante que o prazer.
+            Prefere as companhias que gostem de filosofar sobre a vida e muitas vezes gosta de ficar só. E aí, sozinho, encontra na introspecção a oportunidade de questionar e analisar tudo que o cerca. Você aprendeu a entrar em sintonia com você mesmo, desenvolvendo seus próprios recursos.
+            Você deve aproveitar sua espiritualidade e seu senso refinado de justiça para conquistar seu espaço, mas não pode descuidar, pois tende a entrar em melancolia.
+ 
+ 
+NÚMERO DA ALMA – 8
+            Você aprendeu os benefícios de uma rígida disciplina, inteligência e eficiência desenvolvidas ao longo de suas vidas passadas, quando teve a oportunidade de colocar sua habilidade executiva em ação. Sua ambição e perspicácia o habilitam a organizar empreendimentos de vulto e abraçar grandes causas.
+            Sua determinação e sua vitalidade física contribuem para que exerça uma grande liderança nas atividades de que toma parte. É grande o seu desejo de ocupar altas posições e obter prosperidade material. Você é dotado de muita energia, mas precisa aprender a aceitar seus próprios erros e a não exigir demais de si próprio.
+            Você deve aproveitar toda sua disposição e capacidade de liderança, e controlar o lado materialista, a intolerância, e em especial a sede de poder.
+ 
+ 
+ 
+NÚMERO DA ALMA – 9
+            Você traz a generosidade e o espírito humanitário desenvolvido ao longo das suas vidas passadas. Ficou sempre à disposição dos outros, colocando seus próprios assuntos em segundo plano.
+            Você aprendeu a se doar em favor da comunidade ou daqueles que mais estima. Abraçou uma causa humanitária, dando não só seu afeto como também seu conhecimento e experiência. É prestativo e coloca-se à disposição para ajudar a resolver qualquer problema. Amigo dedicado e bom amante, consegue manter relacionamentos sólidos por muito tempo.
+            Você deve explorar sua capacidade em situações onde pode ser generoso e onde pode usar seus talentos criativos. Controle suas emoções, caso contrário, terá dificuldade em atingir os seus objetivos.
+ 
+ 
+NÚMERO DA ALMA – 11
+            Ao longo de suas vidas passadas, desenvolveu a sabedoria e a compreensão, com forte inclinação à espiritualidade. Aprendeu a ter coragem diante de situações inesperadas.
+            De fato, pode aplicar inúmeras vezes o seu conhecimento espiritual nos mais diversos campos, e à medida que for adquirindo mais sabedoria poderá, com seu idealismo, influenciar as outras pessoas. Poderá ser muito requisitado como conselheiro e confidente.
+            Possui uma visão ampla do mundo à sua volta, devendo dedicar-se à realidade e evitando portanto cair numa vida visionária destituída de senso prático.
+            No entanto, caso você não tenha conseguido viver à altura das vibrações deste Número Mestre, o seu Número de Alma é o 2, que é derivado de 1 + 1.
+ 
+ 
+NÚMERO DE ALMA – 22
+            Ao longo de suas vidas passadas, aprendeu a ter consciência e percepção superiores e a agir como um mestre-construtor querendo interferir na realidade, corrigindo os erros e aparando as arestas, pelo bem e a segurança de todos.
+            Idealista, tem uma inteligência privilegiada, espírito de liderança e uma inabalável capacidade de conquistar grandes vitórias em todos os campos de atuação. Tem sempre como meta contribuir para a construção de um mundo melhor.
+            No entanto, caso você não tenha conseguido viver à altura das vibrações deste Número Mestre, o seu Número de Alma é o 4, que é derivado de 2 + 2.
+ 
+ 
+ 
+NÚMERO DE ALMA – 33
+            Ao longo de suas vidas passadas, aprendeu a dirigir sua vida em função do próximo, iluminando o caminho de todos, sacrificando a sua vida particular e inspirando a criação de organizações com fins humanitários.
+            Paciente e caridoso, é despojado de qualquer apego material. Possui uma humildade impar e uma grande capacidade de compreender o próximo, o que o torna uma pessoa rara.
+No entanto, caso você não tenha conseguido viver à altura das vibrações deste Número Mestre, o seu Número de Alma é o 6, que é derivado de 3 + 3.
+ 
+ 
